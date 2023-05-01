d5c75225b16f70e6cc6688753766e36c8ad34305
Speck <stephan.speck21@gmail.com>
2023-05-01 15:38:11 -0500

    1&2

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs.mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
@@ -22,5 +23,10 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model){
+
+        return null;
+    }
 
 }
